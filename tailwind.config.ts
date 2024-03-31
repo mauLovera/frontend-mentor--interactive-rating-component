d57daf11686c424e05a0052830d024f1f1dbf401
mauLovera <mauricioloveraedu@gmail.com>
2024-03-31 12:30:09 -0400

    update: tw-config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    colors: {
+      primary: {
+        "orange": "hsl(25, 97%, 53%)"
       },
-    },
+      neutral: {
+        "white": "hsl(0, 0%, 100%)",
+        "light-grey": "hsl(217, 12%, 63%)",
+        "medium-grey": "hsl(216, 12%, 54%)",
+        "dark-blue": "hsl(213, 19%, 18%)",
+        "very-dark-blue": "hsl(216, 12%, 8%)",
+      },
+    }
   },
   plugins: [],
 };
